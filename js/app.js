42e4049172f3901871d28ac4af9e5fdf0092f10c
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-01-05 20:06:21 -0500

    I finally got the table to add stuff I think. I have to work on Javascript and also I added what I think is the priorty button and completed button. This should not be too difficult.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let toDoList = [];
 let elements = document.getElementsByClassName("table-row");
 let priorityButtons = document.getElementsByClassName("priority-button");
 let completedButtons = document.getElementsByClassName("completed-button");
-let removeButtons = document.getElementsByClassName("remove-button");
+let removeButtons = document.getElementsByClassName("delete-button");
 
 let priorityChanged;
 let completionChanged;
@@ -12,90 +12,7 @@ let itemRemoved;
 window.onload = function() {
     document.getElementById("task-table").onclick = runModificationFunctions;
 
-    document.getElementById("add-input").onclick = createItem;
-}
-
-const test = function() {
-    alert("test");
-}
-
-const prioritizeItem = function() {
-    priorityChanged = false;
-
-    for (let i = 0; i < priorityButtons.length; i++) {
-        priorityButtons[i].onclick = function() {
-            if (toDoList[i].prioritized === false) {
-                const elementToPrioritize = elements[i];
-                priorityButtons[i].style.color = "red";
-                elements[0].before(elementToPrioritize);
-                toDoList[i].prioritized = true;
-
-                const objectToMove = toDoList[i];
-                toDoList.splice(i, 1);
-                toDoList.unshift(objectToMove);
-                priorityChanged = true;
-            }
-            else if (toDoList[i].prioritized) {
-                const elementToPrioritize = elements[i];
-                priorityButtons[i].style.color = "black";
-                elements[elements.length - 1].after(elementToPrioritize);
-                toDoList[i].prioritized = false;
-
-                const objectToMove = toDoList[i];
-                toDoList.splice(i, 1);
-                toDoList.push(objectToMove);
-                priorityChanged = true;
-            }
-        }
-
-        priorityButtons[i].onclick;
-
-        if (priorityChanged) {
-            break;
-        }
-    }
-}
-
-const markAsComplete = function() {
-    completionChanged = false;
-
-    for (let i = 0; i < completedButtons.length; i++) {
-        completedButtons[i].onclick = function() {
-            if (toDoList[i].completed === false) {
-                completedButtons[i].style.setProperty("text-decoration", "line-through");
-                toDoList[i].completed = true;
-            }
-            else if (toDoList[i].completed) {
-                completedButtons[i].style.setProperty("text-decoration", "none");
-                toDoList[i].completed = false;
-            }
-        }
-
-        completedButtons[i].onclick;
-
-        if (priorityChanged) {
-            break;
-        }
-    }
-}
-
-const removeItem = function() {
-    itemRemoved = false;
-
-    for (let i = 0; i < removeButtons.length; i++) {
-        removeButtons[i].onclick = function() {
-            const elementToRemove = elements[i];
-            elementToRemove.remove();
-            toDoList.splice(i, 1);
-            itemRemoved = true;
-        }
-
-        removeButtons[i].onclick;
-
-        if (itemRemoved) {
-            break;
-        }
-    }
+    document.getElementById("add-input").onclick = createTask;
 }
 
 const runModificationFunctions = function() {
@@ -104,7 +21,7 @@ const runModificationFunctions = function() {
     removeItem();
 }
 
-const createItem = function() {
+const createTask = function() {
     let input = document.getElementById("user-input").value;
     if (input === "") {}
     else {
@@ -119,29 +36,27 @@ const createItem = function() {
             htmlRemoveButton: null
         }
 
+
         toDoList.push(object);
 
-        let x = toDoList.indexOf(object);
+        let i = toDoList.indexOf(object);
 
-        toDoList[x].htmlRow = document.createElement("tr");
-        toDoList[x].htmlRow.setAttribute("class", "table-row");
-        document.getElementById("task-table").append(toDoList[x].htmlRow);
+        toDoList[i].htmlRow = document.createElement("tr");
+        toDoList[i].htmlRow.setAttribute("class", "table-row");
+        document.getElementById("task-table").append(toDoList[i].htmlRow);
 
-        toDoList[x].htmlPriorityButton = document.createElement("td");
-        toDoList[x].htmlPriorityButton.setAttribute("class", "priority-button");
-        toDoList[x].htmlPriorityButton.innerHTML = "!";
-        elements[x].append(toDoList[x].htmlPriorityButton);
+       toDoList[i].htmlPriorityButton = document.createElement("td");
+        toDoList[i].htmlPriorityButton.setAttribute("class", "priority-button");
+        toDoList[i].htmlPriorityButton.innerHTML = "!";
 
-        toDoList[x].htmlText = document.createElement("td");
-        toDoList[x].htmlText.innerHTML = toDoList[x].task;
-        toDoList[x].htmlText.setAttribute("class", "completed-button");
-        elements[x].append(toDoList[x].htmlText);
+        toDoList[i].htmlText = document.createElement("td");
+        toDoList[i].htmlText.innerHTML = toDoList[i].task;
+        toDoList[i].htmlText.setAttribute("class", "completed-button");
 
 
-        toDoList[x].htmlRemoveButton = document.createElement("td");
-        toDoList[x].htmlRemoveButton.setAttribute("class", "remove-button");
-        toDoList[x].htmlRemoveButton.innerHTML = "X";
-        elements[x].append(toDoList[x].htmlRemoveButton);
+        toDoList[i].htmlRemoveButton = document.createElement("td");
+        toDoList[i].htmlRemoveButton.setAttribute("class", "delete-button");
+        toDoList[i].htmlRemoveButton.innerHTML = "X";
     }
     document.getElementById("user-input").value = "";
 }
